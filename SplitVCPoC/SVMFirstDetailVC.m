991fc1aad839282ffa374209ecfa83efc82ed1b6
Amin Siddiqui <amin.siddiqui_work@yahoo.com>
2017-07-19 23:14:34 +0530

    SVMFirstDetailsVC - Implemented method to programmatically create toggle master viewController UI and handling logic - NOTE: navigationItem was not showing up until the viewController was put under it's own navigationController

--- SplitVCPoC/SVMFirstDetailVC.m
+++ SplitVCPoC/SVMFirstDetailVC.m
@@ -19,12 +19,48 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    [self.navigationItem setLeftBarButtonItem:[self createHamburgerBarButton]];
 }
 
 -(IBAction)btnShowMaster_Act:(UIButton *)sender {
     [UIView animateWithDuration:0.27f
                      animations:^{
                          [self.splitViewController setPreferredDisplayMode:UISplitViewControllerDisplayModeAllVisible];
+                     }];
+}
+
+#pragma mark - UISplitViewController Related Helper Methods
+-(UIBarButtonItem *)createHamburgerBarButton {
+    UIBarButtonItem *barButton = [[UIBarButtonItem alloc] init];
+    
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+    [button setFrame:CGRectMake(0, 0, 30, 30)];
+    [button setTitle:@"☰"
+            forState:UIControlStateNormal];
+    [button setTitleColor:[UIColor blackColor]
+                 forState:UIControlStateNormal];
+    
+    [barButton setCustomView:button];
+    [button addTarget:self
+               action:@selector(toggleMasterViewController:event:)
+     forControlEvents:UIControlEventTouchUpInside];
+    
+    return barButton;
+}
+
+-(void)toggleMasterViewController:(UIButton *)sender event:(UIEvent *)event {
+    UISplitViewControllerDisplayMode displayMode = self.splitViewController.displayMode;
+    if (displayMode != UISplitViewControllerDisplayModePrimaryHidden) {
+        displayMode = UISplitViewControllerDisplayModePrimaryHidden;
+    }
+    else {
+        displayMode = UISplitViewControllerDisplayModeAllVisible;
+    }
+    
+    [UIView animateWithDuration:0.27f
+                     animations:^{
+                         [self.splitViewController setPreferredDisplayMode:displayMode];
                      }];
 }
 
