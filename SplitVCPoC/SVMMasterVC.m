2d0f46e72bb54feeaa2ea1bdadb39b6b6c6ac0d4
Amin Siddiqui <amin.siddiqui_work@yahoo.com>
2017-07-19 23:16:13 +0530

    SVMMasterVC - Implemented logic to load an existing detail viewController

--- SplitVCPoC/SVMMasterVC.m
+++ SplitVCPoC/SVMMasterVC.m
@@ -10,9 +10,10 @@
 
 @interface SVMMasterVC () <UITableViewDataSource, UITableViewDelegate>
 {
+    UIViewController *masterViewController;
     IBOutlet UITableView *tvMenu;
     
-    NSArray *arrDatasource;
+    NSMutableArray *arrDatasource;
     NSInteger selectedRow;
 }
 @end
@@ -24,11 +25,20 @@
     // Do any additional setup after loading the view.
     [self.splitViewController setPreferredDisplayMode:UISplitViewControllerDisplayModePrimaryHidden];
     [tvMenu setTableFooterView:[[UIView alloc] initWithFrame:CGRectZero]];
+    masterViewController = self.splitViewController.viewControllers.firstObject;
     
-    arrDatasource = @[@{@"title"                : @"First"
-                        , @"segueIdentifier"    : @"segueSVMFirstDetailVC"}
-                      , @{@"title"              : @"Second"
-                          , @"segueIdentifier"  : @"segueSVMSecondDetailVC"}];
+    [self createDatasource];
+}
+
+-(void)createDatasource {
+    arrDatasource = [[NSMutableArray alloc] init];
+    [arrDatasource addObject:[@{@"title"            : @"First"
+                                , @"segueIdentifier": @"segueSVMFirstDetailVC"
+                                , @"viewController" : self.splitViewController.viewControllers.lastObject
+                                } mutableCopy]];
+    [arrDatasource addObject:[@{@"title"            : @"Second"
+                                , @"segueIdentifier": @"segueSVMSecondDetailVC"
+                                } mutableCopy]];
 }
 
 #pragma mark - UITableViewDataSource
@@ -41,13 +51,14 @@
 }
 
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    NSDictionary *dictOption = arrDatasource[indexPath.row];
+    NSDictionary *dictDetailView = arrDatasource[indexPath.row];
     
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"master option"];
-    [cell.textLabel setText:dictOption[@"title"]];
+    [cell.textLabel setText:dictDetailView[@"title"]];
     
     return cell;
 }
+
 #pragma mark - UITableViewDelegate
 -(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
@@ -62,13 +73,53 @@
         selectedRow = indexPath.row;
         
         //Load Detail viewController
-        NSDictionary *dictOption = arrDatasource[indexPath.row];
-        NSString *strSegueIdentifier = dictOption[@"segueIdentifier"];
+        NSDictionary *dictDetailView = arrDatasource[indexPath.row];
+        NSString *strSegueIdentifier = dictDetailView[@"segueIdentifier"];
         
-        NSLog(@"Loading Detail viewController with segue identifier: %@", strSegueIdentifier);
-        [self performSegueWithIdentifier:strSegueIdentifier
-                                  sender:nil];
+        if ([self shouldPerformSegueWithIdentifier:strSegueIdentifier
+                                            sender:indexPath]) {
+            [self performSegueWithIdentifier:strSegueIdentifier
+                                      sender:indexPath];
+        }
+    }
+    else {
+        NSLog(@"No load needed");
     }
 }
 
+-(BOOL)shouldPerformSegueWithIdentifier:(NSString *)identifier sender:(id)sender {
+    NSIndexPath *indexPath = sender;
+    NSDictionary *dictDetailView = arrDatasource[indexPath.row];
+    
+    if (dictDetailView[@"viewController"]) {
+        /* if detail viewController was previously loaded then do not perform segue
+         and instead update splitViewController manually with the captured detail
+         viewController
+         */
+        
+        UIViewController *detailVC = dictDetailView[@"viewController"];
+        
+        NSLog(@"Loading Existing: %@", detailVC);
+        [self.splitViewController setViewControllers:@[masterViewController
+                                                       , detailVC]];
+        
+        return NO;
+    }
+    
+    return YES;
+}
+
+-(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    UIViewController *detailVC = segue.destinationViewController;
+    
+    /*
+     Capture the detail viewController relative to it's index in the
+     tableView datasource
+     */
+    NSIndexPath *indexPath = sender;
+    NSMutableDictionary *dictDetailView = arrDatasource[indexPath.row];
+    dictDetailView[@"viewController"] = detailVC;
+    
+    NSLog(@"Loading New: %@", detailVC);
+}
 @end
